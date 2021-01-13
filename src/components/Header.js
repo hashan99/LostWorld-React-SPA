c493ef49bb93cadf5e11b10295ac87a5d10ef879
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2021-01-14 02:25:11 +0530

    title implemented

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, IconButton, Toolbar } from '@material-ui/core';
+import { AppBar, Collapse, IconButton, Toolbar } from '@material-ui/core';
 import SortIcon from '@material-ui/icons/Sort';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center', 
+        height: '100vh',
+        fontFamily: 'Nunito',
+    },
     appbar: {
         background: 'none',
-        fontFamily: 'Nunito',
     },
     appbarWrapper: {
         width: '80%',
@@ -19,19 +26,50 @@ const useStyles = makeStyles((theme) => ({
         color: '#fff' ,
         fontSize: '2rem',
     },
+    colorText: {
+        color: '#5AFF3D',
+    },
+    container: {
+        textAlign: 'center',
+    },
+    title: {
+        color: '#fff',
+        fontSize: '4.5rem',
+    },
+    goDown: {
+        color: '#5AFF3D',
+        fontSize: '4rem',
+    },
 }));
 export default function Header() {
     const classes = useStyles();
+    const [checked, setChecked] = useState(false);
+    useEffect(() => {
+        setChecked(true);
+    },[])
     return (
-        <div>
+        <div className={classes.root}>
             <AppBar className={classes.appbar} elevation={0}>
                 <Toolbar className={classes.appbarWrapper}>
-                    <h1 className={classes.appbarTitle}>LostWorld.</h1>
+                    <h1 className={classes.appbarTitle}>
+                        Lost<span className={classes.colorText}>World.</span>
+                    </h1>
                     <IconButton>
                         <SortIcon className={classes.icon} />
                     </IconButton>
                 </Toolbar>
             </AppBar>
+
+            <Collapse in={checked} {...(checked ? { timeout: 1000 } : {})} collapsedHeight={50}>
+            <div className={classes.container}>
+                <h1 className={classes.title}>
+                    Welcome to <br /> Lost <span className={classes.colorText}>World.</span>
+                </h1>
+                <IconButton>
+                    <ExpandMoreIcon className={classes.goDown} />
+                </IconButton>
+            </div>
+            </Collapse>
         </div>
     );
 }
